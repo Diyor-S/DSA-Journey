12378ce8e7d23e6b6a18af51ca71104ebd32c397
Diyor-S <diyorxujasaidaliyev@gmail.com>
2026-09-07 21:03:35 +0500

    Learned new and better approach for validating the anagram

--- Arrays/ValidAnagram-1.py
+++ Arrays/ValidAnagram-1.py
@@ -11,23 +11,40 @@ class Solution:
 
     def isAnagram(self, s: str, t: str) -> bool:
         """
-        The very first idea that comes into mind, to extract each character of the string into 
-        an array, and then sorting that array, and forming the full word again.
+        So today, using a different approach to find identify valid anagram!
+
+        The idea is to use hash tables to track the each characters occurence!
         """
+        # First check the length!
+        if len(s) != len(t):
+            return False
 
-        # Yes, so I am using list comprehensions here, to just get raw list of characters
-        seq_s = [ch for ch in s]
-        seq_t = [ch for ch in t]
+        char_count = {}
 
-        # Just sorting them out
-        seq_s.sort()
-        seq_t.sort()
-
-        # And then forming the characters for both strings again and comparing them.
-        return "".join(seq_s) == "".join(seq_t)
+        # The idea is to fill up the counter!
+        for ch in s:
+            if ch not in char_count:
+                char_count[ch] = 1
+            else:
+                char_count[ch] += 1
         
+        # Then we subtract the counter each time we see familiar character in the second string
+
+        for ch in t:
+            # If ch is not present as the key, then False!
+            if ch not in char_count:
+                return False
+            else:
+                char_count[ch] -= 1 # So now we subtract ch count
+                # Crucial to check whether the value did not drop below 0!!!
+                if char_count[ch] < 0:
+                    return False
+
+        # If all the checks did not result in False, then they are the anagrams!
+
+        return True
 """
-Approaches:
+Approaches 1.
 
 1. Breaking strings into arrays, sorting arrays, and comparing the newly formed sorted characters.
 
@@ -54,8 +71,47 @@ O(2n + 2n) => O(4n) => we drop the 4 constant and get O(n).
 
 Does that mean O(n) is the overall Memory Complexity? Yes.
 
+Solution:
 
-Conclusion:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        The very first idea that comes into mind, to extract each character of the string into 
+        an array, and then sorting that array, and forming the full word again.
+        '''
+
+        # Yes, so I am using list comprehensions here, to just get raw list of characters
+        seq_s = [ch for ch in s]
+        seq_t = [ch for ch in t]
+
+        # Just sorting them out
+        seq_s.sort()
+        seq_t.sort()
+
+        # And then forming the characters for both strings again and comparing them.
+        return "".join(seq_s) == "".join(seq_t)
+
+
+Conclusion 1:
 
 For now, let's stick with this implementation, but this can't be the fastest, so we'll need to explore.
+
+
+# --------------------------------------------------------------------------------------------------------------------------
+
+Approach 2.
+
+
+1. Memory Complexity:
+Allocated hashtable of potentially size length of the string!
+
+O(n)
+
+2. Time Complexity:
+
+Iterated over string of size two times
+We drop the constant 2, so we get O(n)
+
+
+Conclusion: O(n), O(n).
+
 """
